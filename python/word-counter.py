b3927c0ab3d3c566433f601db50ca235606d0790
Kevin Ndolo <kevindev1@outlook.com>
2025-09-22 12:28:34 +0300

    word counter

--- python/word-counter.py
+++ python/word-counter.py
@@ -10,21 +10,56 @@ import os
 def word_counter(filename):
   word_counts = {}
 
-  print(__file__)
-  print(os.path.dirname(__file__))
-  print(os.path.join(os.path.dirname(__file__), filename))
+  # print(__file__)
+  # print(os.path.dirname(__file__))
+  # print(os.path.join(os.path.dirname(__file__), filename))
 
   filepath = os.path.join(os.path.dirname(__file__), filename)
 
+  # Read file
   with open(filepath, "r") as file:
-    text = file.read()
-
-
+      text = file.read()
     # print(text)
 
+  # Normalize file
+  text = text.lower()
+
+  # remove punctuation
+  cleaned_text =""
+  for char in text:
+    if char.isalnum() or char.isspace():
+       cleaned_text += char
+  # print(cleaned_text)
+
+  # Split into words
+  words = cleaned_text.split()
+  print(words)
+
+# Count each word
+  for word in words:
+     if word in word_counts:
+        # current_count = word_counts[word]  # get the current count of the word
+        # new_count = current_count + 1      # add 1 to it
+        # word_counts[word] = new_count      # update the dictionary with the new count
+
+        word_counts[word] += 1
+
+     else:
+        word_counts[word] = 1
 
 
+  # # Define a sort key function
+  # def get_count(item):
+  #   return item[1]
+  
+
+  # Sort by frequency (highest first)
+  # sorted_counts = sorted(word_counts.items(), key=get_count, reverse=True)
+  sorted_counts = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
 
 
+  # Print results
+  for word, count in sorted_counts:
+     print(f"{word}: {count}")
 
 word_counter("word-counter.txt")
